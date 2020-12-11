4e95e152562c1e97daadc8284b564ae7e784df18
Cair0n <lucas.pauzies@hotmail.fr>
2020-12-11 16:26:42 +0100

    Update name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("Readme.md", "r", encoding="utf-8") as readme_descriptor:
     long_description = readme_descriptor.read()
 
 setuptools.setup(
-    name = "mailsender",
+    name = "emailsender",
     version = "1.0",
     author = "Lucas Pauzies",
     author_email = "lucas.pauzies@hotmail.fr",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages = setuptools.find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires = ">=3.6",
